osbuild/images

pkg/oscap: add internal config abstractions
Create simple internal abstractions for OpenSCAP config which can then
be used when creating the `osbuild` stage options.

pkg/customizations/oscap/oscap.go
@@ -40,6 +40,21 @@ const (
 	defaultRHEL9Datastream   string = "/usr/share/xml/scap/ssg/content/ssg-rhel9-ds.xml"
 )
 
+type RemediationConfig struct {
+	Datastream         string
+	ProfileID          string
+	TailoringPath      string
+	CompressionEnabled bool
+}
+
+type TailoringConfig struct {
+	RemediationConfig
+	Filepath   string
+	NewProfile string
+	Selected   []string
+	Unselected []string
+}
+
 func DefaultFedoraDatastream() string {
 	return defaultFedoraDatastream
 }

mvo5/blueprints

pkg/customizations/oscap/oscap.go
@@ -43,6 +43,20 @@ const (
 	DataDir string = "/oscap_data"
 )
 
+type RemediationConfig struct {
+	Datastream         string
+	ProfileID          string
+	TailoringPath      string
+	CompressionEnabled bool
+}
+
+type TailoringConfig struct {
+	RemediationConfig
+	TailoredProfileID string
+	Selected          []string
+	Unselected        []string
+}
+
 func DefaultFedoraDatastream() string {
 	return defaultFedoraDatastream
 }